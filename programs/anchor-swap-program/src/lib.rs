f7009c4b58ba8ddc0134ade46b0cadba6193dec4
saatvik-10 <saatvikmadan529@gmail.com>
2025-09-17 14:17:41 +0530

    fixes

--- programs/anchor-swap-program/src/lib.rs
+++ programs/anchor-swap-program/src/lib.rs
@@ -6,6 +6,7 @@ pub mod state;
 use anchor_lang::prelude::*;
 
 pub use constants::*;
+pub use instructions::*;
 pub use state::*;
 
 declare_id!("qyfaz8Aqt7cDGMor7JJxagUPJybZCUzPAqRGwFb3YXc");
@@ -21,8 +22,26 @@ pub mod anchor_swap_program {
         token_a_offered_amount: u64,
         token_b_wanted_amount: u64,
     ) -> Result<()> {
-        instructions::make_offer::send_offered_token_to_vault(ctx, token_a_offered_amount)?;
+        // Transfer tokens to vault
+        transfer_token(
+            &ctx.accounts.maker_token_account_a,
+            &ctx.accounts.vault,
+            &token_a_offered_amount,
+            &ctx.accounts.token_mint_a,
+            &ctx.accounts.maker,
+            &ctx.accounts.token_program,
+        )?;
 
-        instructions::make_offer::save_offer(ctx, id, token_b_wanted_amount)
+        // Save offer data
+        ctx.accounts.offer.set_inner(Offer {
+            id,
+            maker: ctx.accounts.maker.key(),
+            token_mint_a: ctx.accounts.token_mint_a.key(),
+            token_mint_b: ctx.accounts.token_mint_b.key(),
+            token_b_wanted_amount,
+            bump: ctx.bumps.offer,
+        });
+
+        Ok(())
     }
 }
